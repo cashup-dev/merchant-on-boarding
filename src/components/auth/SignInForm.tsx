4ca12c5cc9d3650589c418f11cc17a671a770dff
nocturnal404 <egiehermawan926@gmail.com>
2025-06-19 23:04:36 +0700

    delete forgot password and sign up

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -106,12 +106,12 @@ export default function SignInForm() {
                     Keep me logged in
                   </span>
                 </div>
-                <Link
+                {/* <Link
                   href="/reset-password"
                   className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
                 >
                   Forgot password?
-                </Link>
+                </Link> */}
               </div>
               <div>
                 <Button
@@ -152,7 +152,7 @@ export default function SignInForm() {
             </div>
           </form>
 
-          <div className="mt-5">
+          {/* <div className="mt-5">
             <p className="text-sm font-normal text-center text-gray-700 dark:text-gray-400 sm:text-start">
               Don&apos;t have an account?{" "}
               <Link
@@ -162,7 +162,7 @@ export default function SignInForm() {
                 Sign Up
               </Link>
             </p>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
