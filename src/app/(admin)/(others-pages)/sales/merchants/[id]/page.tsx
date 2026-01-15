6aa7538a35ae8739102a2cd81d743dde24c9c280
muhammaderlangga99 <muhammaderlangga159@hotmail.com>
2026-01-15 13:52:20 +0700

    solve: error Type { id: string; } is missing the following properties from type Promise any

--- src/app/(admin)/(others-pages)/sales/merchants/[id]/page.tsx
+++ src/app/(admin)/(others-pages)/sales/merchants/[id]/page.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import SalesMerchantDetail from "@/components/sales/SalesMerchantDetail";
 
 type PageProps = {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 };
 
-export default function SalesMerchantDetailPage({ params }: PageProps) {
+export default async function SalesMerchantDetailPage({ params }: PageProps) {
+  const { id } = await params;
   return (
     <div className="space-y-6">
-      <SalesMerchantDetail merchantId={params.id} />
+      <SalesMerchantDetail merchantId={id} />
     </div>
   );
 }
